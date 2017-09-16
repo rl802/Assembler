0b48eea3eec6a1d7a714fa5bf8af71057dcfaaf1
sanjana134 <sg725@snu.edu.in>
2017-09-16 19:04:40 +0530

    Add files via upload

--- Assembler_1.java
+++ Assembler_1.java
@@ -9,12 +9,63 @@ package assembler_1;
  *
  * @author Sanjana13
  */
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class Assembler_1 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    void extract_assembly_code()
+    {
+       BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader("");
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+
+	
+
+    }
+    
+    void evaluate_line()
+    {
+        
+    }
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
     }
     
