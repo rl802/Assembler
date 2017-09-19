7971907b7b6074967bcef34bf0b499841a4ac92a
rl802 <rl802@snu.edu.in>
2017-09-19 18:08:17 +0530

    Add files via upload

--- Assembler_1.java
+++ Assembler_1.java
@@ -15,18 +15,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
 
 public class Assembler_1 {
 
     /**
      * @param args the command line arguments
      */
-    tables t;
+    tables t=new tables();
     converter c;
     
     
@@ -40,20 +39,22 @@ FileInputStream fstream = new FileInputStream("C:\\Users\\rithika\\Documents\\As
 BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
 FileWriter fw=new FileWriter("C:\\Users\\rithika\\Documents\\Assembler\\assembler_1\\obj_code1.txt");
 BufferedWriter bw=new BufferedWriter(fw);
-bw.write("Address Instruction Opcode");
 String strLine;
 
 //Read File Line By Line
 while ((strLine = br.readLine()) != null)   {
   // Split command into separate tokens
-  len=strLine.length();
-  String[] str= strLine.split(",|\\s");
-  
+  String instr=strLine;
+  String[] str= strLine.split(",|\\s|\\r");
+  len=str.length;
   /* Assigning addresses to each instruction */
-  
+  String hex_addr="";
+  if(str[0]==null)
+      continue;
   if(str[1].equals("START")){
       if(str[2]!=null && !str[2].isEmpty())
-            locctr=Integer.parseInt(str[2]);
+            locctr=Integer.parseInt(str[2])-3;
+      continue;
   }
         else{
             if(str[1].equals("RESB"))
@@ -61,37 +62,54 @@ while ((strLine = br.readLine()) != null)   {
             else
                 if(str[1].equals("RESW"))
                     locctr=locctr+(3*Integer.parseInt(str[2]));
+            
             else
                 if(str[1].equals("BYTE"))
-                   locctr++;
-            else
-                locctr=locctr+3;
+                   locctr=locctr+1;
+                    
+                else{
+                    locctr=locctr+3;
+                    
+                    
+                }
+            
         }
-  String hex_addr=Integer.toHexString(locctr);
-  
-  if(str[1].equals("START")||str[0].equals("END"))
-      break;
+  hex_addr=Integer.toHexString(locctr);
+  //System.out.println(hex_addr);
+   if(str[1].equals("START")||str[0].equals("END"))
+          continue;
+  else
+      if(len==3)
+          t.create_symtab(str[0],hex_addr);
+   
+  if(str[1].equals("START")||str[0].equals("END")||str[1].equals("RESB")||str[1].equals("RESW")||str[1].equals("BYTE")||str[1].equals("WORD"))
+      continue;
 else
-      hex_code=t.check_optab(str[0]);
+      if(len==3){
+          
+      hex_code=t.check_optab(str[1]);
+      }
+  else
+          if(len==2)
+              hex_code=t.check_optab(str[0]);
   
-  if(str[2].equals("X"))
+ 
+  
+  if(len==3 && str[2].equals("X"))
       index_bit=1;
   
-    
-    
-    bw.write(hex_addr+" "+strLine+" "+hex_code);
-    
-
-  
-}
+    bw.write(hex_addr+"\t"+instr+"\t"+hex_code+"\t"+index_bit);
+    bw.newLine();
+    }
 
 //Close the input stream
 br.close();
 bw.close();     
-    
+ 
     }
     
     void pass_2(){
+        t.print_symtab();
         BufferedReader br = null;
 		FileReader fr = null;
                 BufferedWriter bw = null;
@@ -108,26 +126,34 @@ bw.close();
 			String sCurrentLine;
 
 			while ((sCurrentLine = br.readLine()) != null) {
-                            StringTokenizer st = new StringTokenizer(sCurrentLine," ");
+                            StringTokenizer st = new StringTokenizer(sCurrentLine,"\t|\\s|\r");
                             int count = st.countTokens();
                               String tok_nm = st.nextToken();
+                              
                               String tok_lb = st.nextToken();
-                              System.out.println(tok_lb);
+                              String tok_hc=  st.nextToken();
+                              String tok_ind= st.nextToken();
                               String obj_2="";
-                              obj_2=t.check_symtab(tok_lb);
-                               if("".equals(obj_2))
-                               {
-                                   bw.write(sCurrentLine);
-                               }
-                               if(st.hasMoreTokens())
-                               {
-                               String tok_x = st.nextToken();
-                               if("1".equals(tok_x))
-                               {
-                                   obj_2=c.addhex("8000",obj_2);
-                               }
-                                bw.write(tok_nm+obj_2);
-                               }
+                              tok_hc=tok_hc+"0000";
+                              int hex= Integer.parseInt(tok_hc,16);
+                              String[] str=tok_lb.split("\\s");
+                              int len=str.length;
+                              if(len==2)
+                              {obj_2=t.check_symtab(str[1]);
+                              System.out.println(obj_2);}
+                              else
+                                obj_2=t.check_symtab(str[2]);  
+                              int obj=Integer.parseInt(obj_2,16);
+                              int address=hex+obj;
+                              if(tok_ind.equals("0")){
+                                  
+                                  bw.write(tok_nm+"\t"+tok_lb+"\t"+Integer.toHexString(address)+"\t");
+                              }
+                              else{
+                                  int fin_address=address+Integer.parseInt("8000",16);
+                                  bw.write(tok_nm+"\t"+tok_lb+"\t"+Integer.toHexString(fin_address)+"\t");
+                              }
+                                
                                
                             //System.out.println(sCurrentLine);
 			}
--- tables.java
+++ tables.java
@@ -5,17 +5,17 @@
  */
 
 package assembler_1;
+import java.io.IOException;
 import java.util.*;
 /**
  *
  * @authors Rithika, Sanjana
  */
 public class tables {
-     Map<String,String> optab;
-     Map<String,String> symtab;
-    tables(){
-        Map<String,String> optab = new HashMap<String,String>();
-         optab.put("ADD","18");
+     public static final Map<String,String> optab;
+     static{
+        optab=new HashMap<String,String>();
+        optab.put("ADD","18");
         optab.put("AND","40");
         optab.put("COMP","28");
         optab.put("DIV","24");
@@ -40,14 +40,17 @@ public class tables {
         optab.put("SUB","1C");
         optab.put("TD","E0");
         optab.put("TIX","2C");
-        optab.put("WD","DC");
-     Map<String,String> symtab= new HashMap<>();   
-    }
+        optab.put("WD","DC");  
+     };
+     public static final Map<String,String> symtab=new HashMap<String,String>();
     
-        
+     
     void create_symtab(String symbol, String addr){
         
         int flag=0;
+        if(symtab.isEmpty())
+            symtab.put(symbol,addr);
+        else{
         for (String sym : symtab.keySet()){
             if(sym.equalsIgnoreCase(symbol)){
                 flag=1;
@@ -56,12 +59,13 @@ public class tables {
         }
             if(flag==0)
             symtab.put(symbol,addr);    
-        
+        } 
     }
     
     String check_optab(String mnemon){
         String addr="";
         if(optab.containsKey(mnemon)){
+            
            addr=optab.get(mnemon);
            
         }
@@ -70,11 +74,27 @@ public class tables {
         return addr;
     }
     
+    void print_optab(){
+    for(String key:optab.keySet())
+        System.out.println(key);
+}
+    
     String check_symtab(String sym){
         String addr="";
-        if(optab.containsKey(sym)){
-           addr=optab.get(sym);
+        if(symtab.containsKey(sym)){
+           addr=symtab.get(sym);
         }
         return addr;
+    }
+    
+     void print_symtab(){
+        for(String key: symtab.values())
+            System.out.println(key);
+    }
+      
+    
+    public static void main(String[] args) throws IOException {
+        tables t1=new tables();
+        t1.print_optab();
     }
 }
